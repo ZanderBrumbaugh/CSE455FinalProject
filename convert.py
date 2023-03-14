625355788e1bd3875b9137b3dde43b05f581941a
Yegor Kuznetsov <yegor@uw.edu>
2023-03-13 23:34:13 -0700

    fix convert:w

--- convert.py
+++ convert.py
@@ -3,6 +3,9 @@ import cv2
 import os
 from tqdm.auto import tqdm
 
+MAX_DESYNC = 0.2
+
+
 
 all_files = os.listdir()
 
@@ -20,20 +23,32 @@ times.sort()
 
 
 
+n_desync = 0
+n_total = 0
+
 for i in tqdm(range(len(times))):
     time, file = times[i]
     if file.startswith('d-'):
         continue
 
-    time_d, file_d = next(filter(lambda f: f[1].startswith('d-'), times[i+1:]))
+    d_iter_result = next(filter(lambda f: f[1].startswith('d-'), times[i+1:]), None)
+    if d_iter_result is None:
+        continue
+
+    time_d, file_d = d_iter_result
 
     image = cv2.imread(file, -1)
     depth = cv2.imread(file_d, -1)
 
-    if abs(time - time_d) > 0.1:
+    n_total += 1
+    if abs(time - time_d) > MAX_DESYNC:
+        n_desync += 1
         continue
 
     os.makedirs(f'data/frame_{i}', exist_ok=True)
     cv2.imwrite(f'data/frame_{i}/image.png', image)
     np.save(f'data/frame_{i}/image_depth.npy', depth)
 
+
+if n_desync / n_total > 0.1:
+    print("warning: high rate of desync, yell at yegor")
